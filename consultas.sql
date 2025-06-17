43672e494b0da2dc363dd5bace40622c729e8a20
carllos39 <carllos2005@outlook.com.br>
2025-06-17 16:41:36 -0300

    atualizar

---
+++ consultas.sql
@@ -0,0 +1,13 @@
+Consultas
+
+insert into cursos (nome,totalhora)values('ingles','60');
+insert into cursos (nome,totalhora)values('espanhol','80');
+
+
+
+insert into aluno (nome,sobrenome,data_nascimento,telefone)values('Maria Silva','Nascimento','2000-05-10','11999999999');
+insert into aluno (nome,sobrenome,data_nascimento,telefone)values('João Souza','Nascimento','1998-08-22','11988888888');
+
+
+insert into turma(data_inicio,horario,nivel,aluno_id)VALUES('2025-06-20','19:00','intermediario','1');
+insert into turma(data_inicio,horario,nivel,aluno_id)VALUES('2025-06-22','19:00','intermediario','2');
